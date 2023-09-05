cd22e32a63ea6e5ac8db898aa9d2c027b6c6eeee
Aivar086 <aivar.alpysbayev@gmail.com>
2023-09-05 15:25:08 +0600

    Создание основы программы

---
+++ ChartExamples.py
@@ -0,0 +1,12 @@
+# Примеры построения графиков
+
+import tkinter as tk
+
+# Создание главное окна
+window = tk. Tk()
+window.geometry("450x450")
+window.title("Примеры построения графиков")
+
+
+# Запуск цикла mainloop
+window.mainloop()
